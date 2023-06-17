dfa27a0362789bcce9e8a9b685206d58fcf52605
Yash Singh <yashsingh0666@gmail.com>
2023-06-17 13:12:36 +0530

    params

---
+++ 02-express-tutorial/json-setup.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const products = require('./data') 
+
+
+app.get('/',(req,res)=>{
+    res.json(products)
+})
+
+
+
+
+app.listen(5000,()=>{
+    console.log("the server is listening on port 5000...")
+})
--- 02-express-tutorial/params-productsPage.js
+++ 02-express-tutorial/params-productsPage.js
@@ -19,35 +19,6 @@ res.json(newProducts)
 })
 
 
-// app.get('/api/products/1',(req,res)=>{
-   
-//     const singleProduct  = products.find((product)=>product.id === 1)
-
-// res.json(singleProduct)
-
-// })
-
-app.get('/api/products/:productID',(req,res)=>{
-//    console.log(req)
-//    console.log(req.params)
-
-// so we saw that params has the info of the product id which in this case is a number what we can do is 
-// we can take the product id from the params and use it === number(productID)
-// usually product id is a string
-
-const {productID} = req.params
-    const singleProduct  = products.find((product)=>product.id === Number(productID))
-    if(!singleProduct){
-        return res.status(404).send("product does not exist")
-    }
-
-return res.json(singleProduct)
-
-})
-
-
-
-
 
 
 app.listen(5000,()=>{
