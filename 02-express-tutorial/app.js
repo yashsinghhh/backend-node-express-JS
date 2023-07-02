5e41b352010665d7f772ec3706382581eee57778
Yash Singh <yashsingh0666@gmail.com>
2023-07-03 01:16:19 +0530

    dead_push

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,40 +7,26 @@ app.get('/', (req,res) => {
 })
 
 app.get("/api/products",(req,res)=>{
-res.json(products)
+// res.json(products)
+const newProducts = products.map((product)=>{
+    const {name, image, price} = product
+    return { name, image ,price}
 })
+res.json(newProducts)
+})
+
+app.get("/api/products/:productID",(req,res) =>{
+    console.log(req)
+    console.log(req.params)
+    const {productID} = req.params
+})
+
 
 app.listen(5000,()=>{
     console.log("server listening to port 5000")
 })
 
 
-// const express = require('express')
-// const app = express()
-// const {products} = require('./data')
-
-
-// app.get('/',(req,res)=>{
-// res.send("<h1>Welcome to the home page, kindly navigate to the products page to view our listed items</h1><a href='/api/products'>Products</a>")
-// })
-
-// app.get('/api/products',(req,res)=>{
-//     // res.json(products)
-// const newProducts = products.map((product)=>{
-//     const {name,id ,image} = product
-//     return {name,id,image}
-// })
-
-// res.json(newProducts)
-
-// })
-
-
-
-
-// app.listen(5000,()=>{
-//     console.log('The server is listening to port 5000')
-// })
 
 
 
