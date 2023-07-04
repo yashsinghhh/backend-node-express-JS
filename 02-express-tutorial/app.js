529797793e1c88d596d83bdc74190c426d3eb660
Yash Singh <yashsingh0666@gmail.com>
2023-07-04 23:19:04 +0530

    bug_check

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,32 +1,10 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const {products} = require('./data')
 
-app.get('/', (req,res) => {
-    res.send("<h1>welcome to home page</h1><a href='/api/products/'>products</a>")
+app.get("/",(req,res)=>{
+    res.status(200).send("<h1>Home Page</h1>")
 })
-
-app.get("/api/products",(req,res)=>{
-// res.json(products)
-const newProducts = products.map((product)=>{
-    const {name, image, price} = product
-    return { name, image ,price}
-})
-res.json(newProducts)
-})
-
-app.get("/api/products/:productID",(req,res) =>{
-    console.log(req)
-    console.log(req.params)
-    const {productID} = req.params
-})
-
 
 app.listen(5000,()=>{
-    console.log("server listening to port 5000")
+    console.log('server is listening to port 5000')
 })
-
-
-
-
-
